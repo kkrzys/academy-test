920bee16b12901b18a9859d23cee21ccd19b5603
kkrzys <klimekkrzys@gmail.com>
2015-02-21 19:23:59 +0100

    added few cities to the seed file (Task no. 6)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+city = City.find_or_create_by(name: 'Berlin', short_name: 'Ber')
+puts 'CREATED/FINDING CITY: ' << city.name
+city = City.find_or_create_by(name: 'Paris')
+puts 'CREATED/FINDING CITY: ' << city.name
+city = City.find_or_create_by(name: 'Madrid', short_name: 'Mad')
+puts 'CREATED/FINDING CITY: ' << city.name
+city = City.find_or_create_by(name: 'Barcelona', short_name: 'Bar')
+puts 'CREATED/FINDING CITY: ' << city.name
+city = City.find_or_create_by(name: 'Warsaw')
+puts 'CREATED/FINDING CITY: ' << city.name
+city = City.find_or_create_by(name: 'Miami', short_name: 'Mi')
+puts 'CREATED/FINDING CITY: ' << city.name
